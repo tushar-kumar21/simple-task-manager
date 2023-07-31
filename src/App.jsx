e06798883e40cf4cfa0cc076d56d28a20c4b75da
tushar-kumar <tusharcr4@gmail.com>
2023-07-31 22:06:33 +0530

    crud

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,91 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(getData());
   const [value, setValue] = useState('');
+  const [toggleBtn, setToggleBtn] = useState(false);
+  const [editToggle, setEditToggle] = useState(null);
 
-  const addData=()=>{
-    !value && setData([...data, value]), setValue("")
+  const addData = () => {
+    if(value.length>0){
+      const allData = { id: new Date().getTime().toString(), name:value}
+      setData([...data, allData]);
+      setValue("");
+    }    
+    else if(value && !toggleBtn){
+        setData(
+          items.map((lists)=>{
+            if(lists.id === editToggle){
+              return { ...lists, name:value} 
+            }
+            return lists;
+          })
+        )
+        setToggleBtn(true)
+        setValue('')
+        setEditToggle(null);
+    }
   }
+  const deleteData = (id) => {
+    setData(
+      data.filter((lists) => {
+        return id !== lists.id;
+      })
+    )
+  }
+
+  const editData=(id)=>{
+  let editListItem = data.find((list)=>{
+   return list.id === id;
+  })
+  console.log(editListItem)
+  setToggleBtn(true)
+  setValue(editListItem.name)
+  setEditToggle(id)
+    }
+
+  const removeAll=()=>{
+    setData([])
+  }
+
+ function getData(){
+    return localStorage.getItem('listsItems') ? JSON.parse(localStorage.getItem('listsItems')) : []
+  }
+
+  useEffect(()=>{
+   localStorage.setItem('listsItems',JSON.stringify(data))
+  },[data])
 
   return (
     <main className='border-2 border-red-500 w-[30rem] aspect-[1.2/1] fixed inset-0 m-auto p-2 flex gap-2 flex-col'>
       <div className='border-2 border-black w-full flex justify-between px-3 rounded-md'>
-        <input type="text" className='outline-none m-2 border-2 rounded-md w-[60%] px-1' value={value} onChange={(e)=>setValue(e.target.value)}/>
+        <input type="text" className='outline-none m-2 border-2 rounded-md w-[60%] px-1' value={value} onChange={(e) => setValue(e.target.value)} />
+        {
+          toggleBtn ? 
+          <button className='text-white border-2 border-black bg-black my-2 text-sm px-4 py-1 rounded-md font-medium transition-all duration-300 hover:bg-transparent hover:text-black active:font-semibold active:scale-[1.05]' onClick={editData}>Edit</button>
+          :
         <button className='text-white border-2 border-black bg-black my-2 text-sm px-4 py-1 rounded-md font-medium transition-all duration-300 hover:bg-transparent hover:text-black active:font-semibold active:scale-[1.05]' onClick={addData}>ADD</button>
+      }
       </div>
-      <section className='border-2 w-full h-full m-auto rounded-md p-2'>
-        <div className='border-2 border-black flex justify-between rounded-md px-2 items-center font-semibold tracking-wide'>
-          <span>Apple</span>
-          <button className='text-white border-2 border-red-600 bg-red-600 my-1 text-sm px-4 py-1 rounded-md font-medium transition-all duration-300 hover:bg-transparent hover:text-red-600 active:font-semibold'>Delete</button>
-        </div>
+      <section className='border-2 w-full h-full m-auto rounded-md p-2 flex flex-col gap-2'>
+        {
+          data.map((lists) => {
+            return (
+              <div className='border-2 border-black flex justify-between rounded-md px-2 items-center font-semibold tracking-wide' key={lists.id}>
+                <span>{lists.name}</span>
+                <div className='flex gap-1'>
+                <button className='text-white border-2 border-green-600 bg-green-600 my-1 text-sm px-4 py-1 rounded-md font-medium transition-all duration-300 hover:bg-transparent hover:text-green-600 active:font-semibold active:scale-[1.05]' onClick={() => editData(lists.id)}>Edit</button>
+                <button className='text-white border-2 border-red-600 bg-red-600 my-1 text-sm px-4 py-1 rounded-md font-medium transition-all duration-300 hover:bg-transparent hover:text-red-600 active:font-semibold active:scale-[1.05]' onClick={() => deleteData(lists.id)}>Delete</button>
+              </div>
+              </div>
+            )
+          })
+        }
       </section>
+      <div><button className='text-white border-2 border-red-600 bg-red-600 my-1 text-sm px-4 py-1 rounded-md font-medium transition-all duration-300 hover:bg-transparent hover:text-red-600 active:font-semibold m-auto block active:scale-[1.05' onClick={removeAll}>Remove All</button></div>
     </main>
   )
 }
